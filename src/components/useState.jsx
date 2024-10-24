b035f402ec82f0ac24d2ca41239e4b48c429d45b
vannala sai <saikrishna.vannala@gmail.com>
2024-10-25 00:03:39 +0530

    second commit

--- src/components/useState.jsx
+++ src/components/useState.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function UseState() {
     const [name, setName] = useState("Black")
     const [isFirstName, setIsFirstName] = useState()
+    const [count, setCount] = useState(0);
 
     const changeName = () => {
       if(isFirstName){
@@ -17,6 +18,8 @@ function UseState() {
       <>
         <h3>My favorite color is {name}!</h3>
         <button onClick={changeName}>{name === "Black" ? "Red" : "Black"}</button>
+        <p>Count : {count}</p>
+        <button onClick={() => setCount(count + 4)}>Increase by 4</button>
       </>
     )
 }
