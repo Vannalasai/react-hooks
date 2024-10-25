1061a4be87ca9042595a79048623f6b366a46319
vannala sai <saikrishna.vannala@gmail.com>
2024-10-25 22:45:40 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-      {/* <UseState /> */}
-      <UseEffect />
+      <UseState />
+      {/* <UseEffect /> */}
     </>
   )
 }
--- src/components/useState.jsx
+++ src/components/useState.jsx
@@ -4,6 +4,7 @@ function UseState() {
     const [name, setName] = useState("Black")
     const [isFirstName, setIsFirstName] = useState()
     const [count, setCount] = useState(0);
+    const [value, setValue] = useState("React")
 
     const changeName = () => {
       if(isFirstName){
@@ -21,12 +22,20 @@ function UseState() {
       setCount(count => count + 1)
     }
 
+    const changeInputValue = (event) => {
+        // console.log(event.target.value)
+        setValue(event.target.value)
+    }
+
     return(
       <>
         <h3>My favorite color is {name}!</h3>
         <button onClick={changeName}>{name === "Black" ? "Red" : "Black"}</button>
         <p>Count : {count}</p>
         <button onClick={increaseCountValue}>Increase by 4</button>
+
+        <h3>React State Input Name: {value}</h3>
+        <input type="text" onChange={() => changeInputValue(event)}/>
       </>
     )
 }
