bbda526ee1bc4bae44932cab5680ad6b3eeff31a
vannala sai <saikrishna.vannala@gmail.com>
2024-10-26 23:59:24 +0530

    five commit

--- src/components/useRef.jsx
+++ src/components/useRef.jsx
@@ -4,10 +4,15 @@ import { useEffect, useRef, useState } from "react";
 function UseRef(){
   const [value, setValue] = useState(0)
   const count = useRef(0)
+  const inputValue = useRef("")
 
   useEffect(() => {
     count.current = count.current + 1;
   })
+
+  const btnClicked = () => {
+    console.log(inputValue.current)
+  }
 
   return(
     <>
@@ -15,6 +20,8 @@ function UseRef(){
       <h2>{value}</h2>
       <button onClick={() => setValue(prev => prev+1)}>+1</button>
       <h2>Rendering Times {count.current}!</h2>
+      <input type="text" ref={inputValue}/>
+      <button onClick={btnClicked}>Click here!</button>
     </>
   )
 } 
